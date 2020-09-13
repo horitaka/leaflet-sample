dba1f7cd5fcf5b601db1a6d5eb06bc5599186ad6
TO <takahiro.ohori@gmail.com>
2020-09-13 11:50:19 +0900

    update 地図切り替えの追加

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-window.addEventListener('load', () => {
 
-  var map = L.map('mapid', {
+window.addEventListener('load', () => {
+  let mapImage;
+
+  const map = L.map('mapid', {
     crs: L.CRS.Simple,
     zoomControl: false,
   });
@@ -8,7 +10,10 @@ window.addEventListener('load', () => {
   var bounds = [[0, 0], [640, 640]];
   map.setMaxBounds(bounds)
 
-  var image = L.imageOverlay('./images/map-4348394_1280.png', bounds).addTo(map);
+  // var map1 = L.imageOverlay('./images/map1.png', bounds).addTo(map);
+  // var map2 = L.imageOverlay('./images/map2.png', bounds).addTo(map);
+  // map2.bringToBack()
+  mapImage = setMapImage(map, 'story1', bounds)
 
   L.control.zoom({
     position: 'topright'
@@ -16,5 +21,34 @@ window.addEventListener('load', () => {
 
   map.fitBounds(bounds);
 
+
+  const buttonStory1 = document.getElementById('button-story1')
+  const buttonStory2 = document.getElementById('button-story2')
+
+  buttonStory1.addEventListener('click', () => {
+    removeMapImage(mapImage)
+    mapImage = setMapImage(map, 'story1', bounds)
+  })
+
+  buttonStory2.addEventListener('click', () => {
+    removeMapImage(mapImage)
+    mapImage = setMapImage(map, 'story2', bounds)
+  })
+
 })
 
+function removeMapImage(mapImage) {
+  mapImage.remove()
+}
+
+function setMapImage(map, mapType, bounds) {
+  // map.remove()
+
+  const imagePath = mapType === 'story1'
+    ? './images/map1.png'
+    : './images/map2.png'
+
+  const image = L.imageOverlay(imagePath, bounds).addTo(map);
+
+  return image
+}
