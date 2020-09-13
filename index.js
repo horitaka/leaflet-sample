deec40d7d900bdfbabf22c2a8b75acc89b008822
TO <takahiro.ohori@gmail.com>
2020-09-13 11:25:57 +0900

    update 拡大縮小ボタンのスタイル

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 window.addEventListener('load', () => {
 
   var map = L.map('mapid', {
-    crs: L.CRS.Simple
+    crs: L.CRS.Simple,
+    zoomControl: false,
   });
 
-  var bounds = [[0, 0], [1000, 1000]];
+  var bounds = [[0, 0], [640, 640]];
   var image = L.imageOverlay('./images/map-4348394_1280.png', bounds).addTo(map);
+
+  L.control.zoom({
+    position: 'topright'
+  }).addTo(map)
 
   map.fitBounds(bounds);
 
